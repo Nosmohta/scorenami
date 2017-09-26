b794a746288bd762d03b590124178af453a9d947
Andrew Thomson <andrew@relaylabs.co>
2017-09-26 15:37:40 -0600

    Client-side code changed to accept gameId property.

--- packages/scorenami-client/src/components/game-summary.js
+++ packages/scorenami-client/src/components/game-summary.js
@@ -29,24 +29,24 @@ const GameSummary = props => {
         <div className="game-summary-team" style={gameSummaryTeamStyles}>
           <span className="game-summary-logo" style={logoStyles}>
             <img
-              className={`game-summary-logo-${away.toLowerCase()}`}
-              src={getTeamLogo(away)}
-              alt={away.toLowerCase()}
+              className={`game-summary-logo-${away.team.toLowerCase()}`}
+              src={getTeamLogo(away.team)}
+              alt={away.team.toLowerCase()}
               width="20px"
             />
           </span>
-          {nflTeams[away].displayName} <strong>{awayScore}</strong>
+          {nflTeams[away.team].displayName} <strong>{awayScore}</strong>
         </div>
         <div className="game-summary-team" style={gameSummaryTeamStyles}>
           <span className="game-summary-logo" style={logoStyles}>
             <img
-              className={`game-summary-logo-${home.toLowerCase()}`}
-              src={getTeamLogo(home)}
-              alt={home.toLowerCase()}
+              className={`game-summary-logo-${home.team.toLowerCase()}`}
+              src={getTeamLogo(home.team)}
+              alt={home.team.toLowerCase()}
               width="20px"
             />
           </span>
-          {nflTeams[home].displayName} <strong>{homeScore}</strong>
+          {nflTeams[home.team].displayName} <strong>{homeScore}</strong>
         </div>
         {final && <span style={finalStyles}>Final</span>}
       </ListItem>
